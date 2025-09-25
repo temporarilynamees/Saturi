baaf470f2e1a887b01360dfc66bd81fc58460b31
jonggu2020 <75966566+jonggu2020@users.noreply.github.com>
2025-09-25 17:56:35 +0900

    Update 04_classification_decision_tree.py

--- AI/04_classification_decision_tree.py
+++ AI/04_classification_decision_tree.py
@@ -22,7 +22,7 @@ model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
 # 5. 모델 평가
-# 실제 값(y_test)과 예측 값(predictions)을 비교하여 정확도 계산..
+# 실제 값(y_test)과 예측 값(predictions)을 비교하여 정확도 계산....
 accuracy = accuracy_score(y_test, predictions)
 print(f"예측 결과 (처음 5개): {predictions[:5]}")
 print(f"실제 값 (처음 5개): {y_test[:5]}")

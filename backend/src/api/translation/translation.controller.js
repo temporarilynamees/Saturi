3eaea57ddf82f39a6c336bb86cf86555dd17ffd4
sungchan12 <chanyouunng1@gmail.com>
2025-09-30 22:05:27 +0900

    9/30

--- backend/src/api/translation/translation.controller.js
+++ backend/src/api/translation/translation.controller.js
@@ -1,11 +1,26 @@
-const service = require('./service');
+const service = require('./translation.service');
 
+/**
+ * @param {} req 
+ * @param {} res 
+ * @param {} next 
+ * @returns 
+ */
 async function translateSentence(req, res, next) {
     try {
         const dialectSentence = req.body.sentence;
+        if (!dialectSentence || typeof dialectSentence !== 'string') {
+            return res.status(400)
+            .json({message: '문장이 필요합니다.'});
+        }
         const translateSentence = await service.translateSentence(dialectSentence);
+        return res.status(200)
+        .json({sentence: translateSentence});
     }
     catch (error) {
         next(error);
     }
+}
+module.exports = {
+    translateSentence
 }

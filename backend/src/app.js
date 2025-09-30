699ba24dc9498ce8883476af9d57af31cbbad13f
sungchan12 <chanyouunng1@gmail.com>
2025-09-30 22:05:17 +0900

    9/30

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,19 +5,13 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use('/', express.static());
-app.use('/api/translate',);
-app.use('/api/tts',);
-app.use('/api/',);
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/api/translation', require('./api/translation/translation.router'));
 
 /**
  * 정적인 html 파일 반환하는 라우트 핸들러들
  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join());
-});
-
-app.get('/videos/:videouid', (req, res) => {
     res.sendFile(path.join());
 });
 

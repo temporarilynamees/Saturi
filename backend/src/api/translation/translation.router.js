191bc9148b156214861c86d78562a05e419cc0cd
sungchan12 <chanyouunng1@gmail.com>
2025-09-30 22:05:40 +0900

    9/30

--- backend/src/api/translation/translation.router.js
+++ backend/src/api/translation/translation.router.js
@@ -1,7 +1,7 @@
-const controller = require('./controller');
+const controller = require('./translation.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', controller.translateSentence)
 
 module.exports = router;

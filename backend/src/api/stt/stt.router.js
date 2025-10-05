8e93552b36d557d348d97f3c816e4332b89e8126
sungchan12~ <chanyouunng1@gmail.com>
2025-10-05 17:20:50 +0900

    router

--- backend/src/api/stt/stt.router.js
+++ backend/src/api/stt/stt.router.js
@@ -1,7 +1,20 @@
 const controller = require('./stt.controller');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
-router.post('/', controller.speechToText);
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../../uploads'));
+    },
+    filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, 'audio-' + uniqueSuffix + '.wav');
+    }
+});
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('audio'), controller.speechToText);
 
 module.exports = router;

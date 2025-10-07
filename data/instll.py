eb59e4587056a1cc39eb3f875e4d2e1d5ed7a630
jonggu2020 <75966566+jonggu2020@users.noreply.github.com>
2025-10-07 21:28:35 +0900

    Create instll.py

---
+++ data/instll.py
@@ -0,0 +1,3 @@
+# TPU 관련 라이브러리 및 기타 필수 패키지 설치
+!pip install cloud-tpu-client==0.10 torch==2.0.0 torchvision==0.15.1 torch_xla[tpu]~=2.0.0 --quiet
+!pip install transformers[torch] sentencepiece datasets openpyxl wandb

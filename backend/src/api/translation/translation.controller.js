728ce16b5d6e2ce94477b101ea81446d7e788491
sungchan12 <chanyouunng1@gmail.com>
2025-10-09 19:50:13 +0900

    add body paratmeter

--- backend/src/api/translation/translation.controller.js
+++ backend/src/api/translation/translation.controller.js
@@ -10,14 +10,14 @@ async function translateSentence(req, res, next) {
     try {
         const dialectSentence = req.body.sentence;
         const direction = req.body.direction;
-        
+
         if (!dialectSentence || typeof dialectSentence !== 'string') {
             return res.status(400)
-            .json({message: '문장이 필요합니다.'});
+                .json({message: '문장이 필요합니다.'});
         }
         const translateSentence = await service.translateSentence(dialectSentence, direction);
         return res.status(200)
-        .json({translation: translateSentence});
+            .json({translation: translateSentence});
     }
     catch (error) {
         next(error);

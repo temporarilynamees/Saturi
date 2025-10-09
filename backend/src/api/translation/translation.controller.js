40c3df27cd458a69cb322c565264c90288dbea10
sungchan12 <chanyouunng1@gmail.com>
2025-10-09 19:48:46 +0900

    add translation.controller

--- backend/src/api/translation/translation.controller.js
+++ backend/src/api/translation/translation.controller.js
@@ -9,11 +9,13 @@ const service = require('./translation.service');
 async function translateSentence(req, res, next) {
     try {
         const dialectSentence = req.body.sentence;
+        const direction = req.body.direction;
+        
         if (!dialectSentence || typeof dialectSentence !== 'string') {
             return res.status(400)
             .json({message: '문장이 필요합니다.'});
         }
-        const translateSentence = await service.translateSentence(dialectSentence);
+        const translateSentence = await service.translateSentence(dialectSentence, direction);
         return res.status(200)
         .json({translation: translateSentence});
     }

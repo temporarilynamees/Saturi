47728e8eb2e7bd39178523daad77232df5592109
jonggu2020 <75966566+jonggu2020@users.noreply.github.com>
2025-10-11 22:42:13 +0900

    Update get_model_path.py

--- TTS/get_model_path.py
+++ TTS/get_model_path.py
@@ -7,7 +7,7 @@ model_name = "tts_models/ko/kss/vits"
 # 모델 매니저를 통해 모델 다운로드 및 경로 확인
 manager = ModelManager()
 
-# 모델을 다운로드하고, 모델 파일(.pth)과 설정 파일(.json)의 경로를 가져옵니다.
+# 모델을 다운로드하고, 모델 파일(.pth)과 설정 파일(.json)의 경로를 가져옵니다.---
 model_path, config_path, _ = manager.download_model(model_name)
 
 print(f"'{model_name}' 모델의 경로를 찾았습니다.")
